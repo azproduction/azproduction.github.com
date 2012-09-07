635fb0c976371511b844e89965a7c0b146e63ff1
azproduction <azazel.private@gmail.com>
2012-09-07 20:21:19 +0600

    deploy to nodejitsu

---
+++ server.js
@@ -0,0 +1,31 @@
+// Nodejitsu
+var httpServer = require('http-server');
+
+var port = 80,
+    host = '0.0.0.0';
+
+var options = {
+    root: '.',
+    autoIndex: true,
+    cache: true
+};
+
+function onListening() {
+    console.log('Starting up http-server, serving '
+        + server.root
+        + ' on port: '
+        + port.toString());
+}
+
+var server = httpServer.createServer(options);
+server.listen(port, host, onListening);
+
+if (process.platform !== 'win32') {
+    //
+    // Signal handlers don't work on Windows.
+    //
+    process.on('SIGINT', function () {
+        console.log('http-server stopped.');
+        process.exit();
+    });
+}
